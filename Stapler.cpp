a3f951a25ffa6be21f7ba0e049289d06b6b10062
Vladislav Sychugin <4myebay@mail.ru>
2023-11-26 15:14:37 +0500

    finish

--- Stapler.cpp
+++ Stapler.cpp
@@ -3,11 +3,10 @@
 
 int main()
 {
-	setlocale(LC_ALL, "RU_ru");
-	std::cout << "Сшиватель дробных чисел\n";
+	std::cout << "Stapler\n";
 	std::string whole_part, frac_part;
-	std::cout << "Введите целую и дробную часть через пробел: ";
+	std::cout << "Input whole and fractional parts separated by a space: ";
 	std::cin >> whole_part >> frac_part;
-	double result = std::stod(whole_part + ',' + frac_part);
-	std::cout << "Получилось: " << result;
+	double result = std::stod(whole_part + '.' + frac_part);
+	std::cout << "Result: " << result;
 }
