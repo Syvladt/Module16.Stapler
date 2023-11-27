86c1b91274da91d48f02e698b2a14211c7ea8db4
Vladislav Sychugin <4myebay@mail.ru>
2023-11-27 06:30:10 +0500

    finish

--- Stapler.cpp
+++ Stapler.cpp
@@ -8,6 +8,6 @@ int main()
 	std::cout << "Input whole and fractional parts separated by a space: ";
 	std::cin >> whole_part >> frac_part;
 	double result = std::stod(whole_part + '.' + frac_part);
-	std::cout.precision(20);
+	std::cout.precision(whole_part.size() + frac_part.size() + 1);
 	std::cout << "Result: " << result;
 }
